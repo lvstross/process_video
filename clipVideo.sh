874a57ca1120f54485b94df7094630f54364b076
Levi Gonzales <lvstross@gmail.com>
2020-04-17 22:51:21 -0600

    Added script that will clip video by time stamp.

---
+++ clipVideo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "inputVideo: $1"
+echo "outputVideo: $2"
+echo "startTime: $3"
+echo "endTime: $4"
+
+if [ "$1" != "" ]; then
+  # ffmpeg -i ./downloads/video1.mp4 -ss 2:26.12 -strict -2 -to 2:34.15 ./clips/clip11.mp4
+  ffmpeg -i $1 -ss $3 -strict -2 -to $4 $2
+else
+  echo "Oops! Need arguments."
+fi
