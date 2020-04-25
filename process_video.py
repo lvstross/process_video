abae30641e8cbf1eaa57b1ca6c6415cbb3794165
Levi Gonzales <lvstross@gmail.com>
2020-04-24 23:44:54 -0600

    Made upload script dynamic. Saving more essential data on uploaded videos now.

--- process_video.py
+++ process_video.py
@@ -2,6 +2,7 @@
 import time
 import json
 import random
+import sys
 from os import listdir
 from os.path import isfile, join
 from moviepy.editor import VideoFileClip, concatenate_videoclips
@@ -10,39 +11,55 @@ from moviepy.editor import VideoFileClip, concatenate_videoclips
 path = "./clips/"
 clipFilePaths = [path + f for f in listdir(path) if isfile(join(path, f))]
 
-# Save used files to json
+# Get past upload and clips data from json file
 data = {}
-with open('usedFiles.json') as json_file:
+with open("./json/usedFiles.json") as json_file:
     data = json.load(json_file)
 
-# Get passed files used
+# Get past files used
+usedFiles = data["usedFiles"]
+availableFiles = []
+for file in clipFilePaths:
+    hasClipBeenUsed = file in usedFiles
+    if not hasClipBeenUsed:
+        availableFiles.append(file)
 
-# Make buffer for while loop so that if there are no more
-# clips to be used, stop the loop and log the failure
-# allow index failure up to 30 trys. If one is found
-# start buffer back at 0
+# Make sure there are 50 available clips mininum
+if len(availableFiles) < 50:
+    print("Process Video Stopped! 50 files required. Only ", len(availableFiles), " are available.")
+    sys.exit()
 
-# Get list of video object until duration has passed 6 minutes
+# Get list of video objects until duration has passed 6 minutes
+# If video search by available index fails 30 times in a row, script will stop
 indexes = []
 clipsUsed = []
 videoClips = []
+buffer = 0
 totalDuration = 0
 while totalDuration < 360:
     # Generate Random index and get clip by index
-    n = random.randint(0, len(clipFilePaths) - 1)
+    n = random.randint(0, len(availableFiles) - 1)
     hasIndexAlready = n in indexes
     # if index has not been used already, save video clip
     if not hasIndexAlready:
         # Use index data
         indexes.append(n)
+
         # Use clip file path data
-        clip = clipFilePaths[n]
+        clip = availableFiles[n]
         clipsUsed.append(clip)
-        data['usedFiles'].append(clip)
+        data["usedFiles"].append(clip)
+
         # Use video object data
         videoObject = VideoFileClip(clip).resize(newsize=(1280,720))
         videoClips.append(videoObject)
         totalDuration += videoObject.duration
+        buffer = 0
+    else:
+        buffer += 1
+        if buffer > 29:
+            print("Buffer Point Exceeded: Clip index search was too long. Add more clips!")
+            sys.exit()
 
 # Generate video with dynamic file name using current unix timestamp
 timeStamp = repr(time.time())
@@ -55,12 +72,19 @@ finalClip.write_videofile(
     audio_codec="aac"
 )
 
-# Get generated video data to save to json
-data['generatedUploads'].append({
-    'fileName': fileName,
-    'totalDuration': totalDuration,
-    'clips': clipsUsed
+# Get generated video data to save to json file
+data["generatedUploads"].append({
+    "fileName": fileName,
+    "title": "",
+    "description": "",
+    "uploaded": False,
+    "url": "",
+    "totalDuration": round(totalDuration / 60, 2),
+    "usedCount": len(clipsUsed),
+    "currentAvailableCount": len(availableFiles) - len(clipsUsed),
+    "clips": clipsUsed
 })
 
-with open('usedFiles.json', 'w') as json_file:
+# Write video and clip data to json file
+with open("./json/usedFiles.json", "w") as json_file:
     json.dump(data, json_file, indent=2)
--- uploadVideo.js
+++ uploadVideo.js
@@ -4,88 +4,123 @@
  *
  * To run this script you have to create OAuth2 credentials and download them
  * as JSON and replace the `credentials.json` file. Then install the
- * dependencies:
- *
- * npm i r-json lien opn bug-killer
- *
- * Don't forget to run an `npm i` to install the `youtube-api` dependencies.
  * */
 require('dotenv/config');
-const Youtube = require("youtube-api")
-    , fs = require("fs")
-    , readJson = require("r-json")
-    , Lien = require("lien")
-    , Logger = require("bug-killer")
-    , opn = require("opn")
-    , prettyBytes = require("pretty-bytes")
-    ;
+const Youtube = require("youtube-api"),
+  fs = require("fs"),
+  readJson = require("r-json"),
+  Lien = require("lien"),
+  Logger = require("bug-killer"),
+  opn = require("opn"),
+  prettyBytes = require("pretty-bytes");
 
 // I downloaded the file from OAuth2 -> Download JSON
 const CREDENTIALS = readJson(`./json/${process.env.YOUTUBE_CREDS}`);
+const USED_FILES = readJson('./json/usedFiles.json');
+
+let firstAvailableIndex = -1;
+const generatedUploads = USED_FILES.generatedUploads.filter((upload, index) => {
+  if (!upload.uploaded && firstAvailableIndex === -1) {
+    firstAvailableIndex = index;
+  }
+  return !upload.uploaded;
+});
+
+const nextFileToUpload = generatedUploads[0];
 
 // Init lien server
 let server = new Lien({
-    host: "localhost"
-  , port: 5000
+  host: "localhost",
+  port: 5000
 });
 
 // Authenticate
 // You can access the Youtube resources via OAuth2 only.
 // https://developers.google.com/youtube/v3/guides/moving_to_oauth#service_accounts
 let oauth = Youtube.authenticate({
-    type: "oauth"
-  , client_id: CREDENTIALS.web.client_id
-  , client_secret: CREDENTIALS.web.client_secret
-  , redirect_url: CREDENTIALS.web.redirect_uris[0]
+  type: "oauth",
+  client_id: CREDENTIALS.web.client_id,
+  client_secret: CREDENTIALS.web.client_secret,
+  redirect_url: CREDENTIALS.web.redirect_uris[0]
 });
 
 opn(oauth.generateAuthUrl({
-    access_type: "offline"
-  , scope: ["https://www.googleapis.com/auth/youtube.upload"]
+  access_type: "offline",
+  scope: ["https://www.googleapis.com/auth/youtube.upload"]
 }));
 
-// Handle oauth2 callback
 server.addPage("/oauth2callback", lien => {
-    Logger.log("Trying to get the token using the following code: " + lien.query.code);
-    oauth.getToken(lien.query.code, (err, tokens) => {
+  Logger.log("Trying to get the token using the following code: " + lien.query.code);
+  oauth.getToken(lien.query.code, (err, tokens) => {
+    if (err) {
+      lien.lien(err, 400);
+      return Logger.log(err);
+    }
 
-        if (err) {
-            lien.lien(err, 400);
-            return Logger.log(err);
+    Logger.log("Got the tokens.");
+    oauth.setCredentials(tokens);
+    lien.end("The video is being uploaded. Check out the logs in the terminal.");
+
+    var req = Youtube.videos.insert({
+      resource: {
+        snippet: {
+          title: USED_FILES.titles.splice(0, 1),
+          description: "Thank you for watching! Which clip made you laugh? Let me know in the comments. Like and subscribe if you enjoyed the video.",
+          tags: ['memes', 'challenge', 'funny', 'laugh'],
+        },
+        status: {
+          privacyStatus: "private"
         }
-
-        Logger.log("Got the tokens.");
-
-        oauth.setCredentials(tokens);
-
-        lien.end("The video is being uploaded. Check out the logs in the terminal.");
-
-        var req = Youtube.videos.insert({
-            resource: {
-                // Video title and description
-                snippet: {
-                    title: "Testing YoutTube API NodeJS module"
-                  , description: "Test video upload via YouTube API"
-                }
-                // I don't want to spam my subscribers
-              , status: {
-                    privacyStatus: "private"
-                }
-            }
-            // This is for the callback function
-          , part: "snippet,status"
-
-            // Create the readable stream to upload the video
-          , media: {
-                body: fs.createReadStream("./uploads/upload-1587335519187.mp4")
-            }
-        }, (err, data) => {
-            console.log("Done.");
-            process.exit();
-        });
-
-        setInterval(function () {
-            Logger.log(`${prettyBytes(req.req.connection._bytesDispatched)} bytes uploaded.`);
-        }, 250);
+      },
+      part: "snippet,status",
+      media: {
+        body: fs.createReadStream(`./uploads/${nextFileToUpload.fileName}`)
+      }
+    },
+    (err, data) => {
+      if (err) {
+        console.log('Error: ', err);
+        process.exit();
+      }
+      console.log('Video Uploaded Successfully!');
+      USED_FILES.generatedUploads[firstAvailableIndex].uploaded = true;
+      USED_FILES.generatedUploads[firstAvailableIndex].url = `https://www.youtube.com/watch?v=${data.id}`;
+      USED_FILES.generatedUploads[firstAvailableIndex].title = data.snippet.title;
+      USED_FILES.generatedUploads[firstAvailableIndex].description = data.snippet.description;
+      fs.writeFileSync('./json/usedFiles.json', JSON.stringify(USED_FILES, null, 2));
+      process.exit();
     });
+
+    setInterval(() => {
+      Logger.log(`${prettyBytes(req.req.connection._bytesDispatched)} bytes uploaded.`);
+    }, 250);
+  });
 });
+
+// Data returned after video
+// Data:  { kind: 'youtube#video',
+//   etag: '"nxOHAKTVB7baOKsQgTtJIyGxcs8/PSSEK8twvSrbRXR0qIHl3T3mDBA"',
+//   id: 'Il8HQUcVjvQ',
+//   snippet:
+//    { publishedAt: '2020-04-25T04:24:30.000Z',
+//      channelId: 'UCNrIi_92Y0GKWpO0SNTZ_VQ',
+//      title: 'Test Video',
+//      description:
+//       'Thank you for watching! Which clip made you laugh? Let me know in the comments. Like and subscriber if you enjoyed yourself.',
+//      thumbnails: { default: [Object], medium: [Object], high: [Object] },
+//      channelTitle: 'More Over Memes',
+//      tags: [ 'memes', 'challenge', 'funny', 'laugh' ],
+//
+//      categoryId: '22',
+//      liveBroadcastContent: 'none',
+//      localized:
+//       { title: 'Test Video',
+//         description:
+//          'Thank you for watching! Which clip made you laugh? Let me know in the comments. Like and subscriber if you enjoyed yourself.' } },
+//   status:
+//    { uploadStatus: 'uploaded',
+//      privacyStatus: 'private',
+//      license: 'youtube',
+//      embeddable: true,
+//      publicStatsViewable: true }
+//   }
